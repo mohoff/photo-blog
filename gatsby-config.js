c4b2115b05962a3994895697bd9b02421fc63556
mohoff <mohoff@web.de>
2019-06-05 20:14:23 +0200

    fix: fix production build by adding material-ui plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Random Access Memories`,
+    title: `Photo Blog`,
     author: `Moritz Hoffmann`,
   },
   plugins: [
@@ -22,22 +22,8 @@ module.exports = {
         path: `${__dirname}/data`,
       },
     },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 800,
-            },
-          },
-        ],
-      }
-    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-material-ui`,
     `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-plugin-sharp`,
